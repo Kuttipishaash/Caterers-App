1f1bb3e386b490b6d694452c79f0b8c8966d91c3
Prabhashankar Kannapan <shankar75031@gmail.com>
2020-04-17 23:35:25 +0530

    dependency updates and flipper fixes

--- app/src/main/java/com/caterbazar/App.java
+++ app/src/main/java/com/caterbazar/App.java
@@ -4,7 +4,8 @@ import android.app.Application;
 
 import com.facebook.flipper.android.AndroidFlipperClient;
 import com.facebook.flipper.android.utils.FlipperUtils;
-import com.facebook.flipper.core.FlipperClient;
+import com.facebook.flipper.android.FlipperClient;
+//import com.facebook.flipper.core.FlipperClient;
 import com.facebook.flipper.plugins.inspector.DescriptorMapping;
 import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin;
 import com.facebook.flipper.plugins.sharedpreferences.SharedPreferencesFlipperPlugin;
@@ -14,11 +15,11 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        SoLoader.init(this, false);
 
-        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
-            final FlipperClient client = AndroidFlipperClient.getInstance(this);
-            client.addPlugin(new InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()));
+        SoLoader.INSTANCE.init(this, false);
+        if (BuildConfig.DEBUG && FlipperUtils.INSTANCE.shouldEnableFlipper(this)) {
+            final FlipperClient client = AndroidFlipperClient.INSTANCE.getInstance(this);
+            client.addPlugin(new InspectorFlipperPlugin(this, DescriptorMapping.INSTANCE.withDefaults()));
             client.addPlugin(new SharedPreferencesFlipperPlugin(this));
             client.start();
         }
